9820f0a1aeaa7133fdccbb6c64afebb99b23b881
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-12 18:28:56 +0000

    When running function peep() moles pop up for the random time selected

--- scripts.js
+++ scripts.js
@@ -11,9 +11,17 @@ function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
     if (hole === lastHole) {
-        console.log("Same hole");
         return randomHole(holes);
     }
     lastHole = hole;
     return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+    }, time);
 }
